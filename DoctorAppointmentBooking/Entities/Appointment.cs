ce40a21364dd2f656714b85e74bf325fb0a3fda4
huangshengsimtech <huangsheng.sg@gmail.com>
2023-06-16 23:10:03 +0800

    Create entities: Appointment and DoctorTimeSlot

---
+++ DoctorAppointmentBooking/Entities/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoctorAppointmentBooking.Entities
+{
+    public class Appointment
+    {
+        [Required] public Guid Id { get; set; }
+        [Required] public Guid SlotId { get; set; }
+        [Required] public Guid PatientId { get; set; }
+        [Required] public string PatientName { get; set; }
+        [Required] public Date ReservedAt { get; set; }
+    }
+}
---
+++ DoctorAppointmentBooking/Entities/DoctorTimeSlot.cs
@@ -0,0 +1,15 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoctorAppointmentBooking.Entities
+{
+    public class DoctorTimeSlot
+    {
+        [Required] public Guid Id { get; set; }
+        [Required] public Date Time { get; set; }
+        [Required] public Guid DoctorId { get; set; }
+        [Required] public string DoctorName { get; set; }
+        [Required] public bool IsReserved { get; set; }
+        [Required] public decimal Cost { get; set; }
+    }
+}
