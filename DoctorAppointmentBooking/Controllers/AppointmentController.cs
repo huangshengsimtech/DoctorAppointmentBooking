300a0a8d93dffe1164e21da995960de58276bb91
huangshengsimtech <huangsheng.sg@gmail.com>
2023-06-18 13:59:04 +0800

    Add Controllers: DoctorTimeSlotController and AppointmentController.

---
+++ DoctorAppointmentBooking/Controllers/AppointmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DoctorAppointmentBooking.Entities;
+using DoctorAppointmentBooking.Services;
+
+namespace DoctorAppointmentBooking.Controllers
+{
+    [Route("/doctortimeslots")]
+    public class AppointmentController
+    {
+    }
+}
---
+++ DoctorAppointmentBooking/Controllers/DoctorTimeSlotController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DoctorAppointmentBooking.Entities;
+using DoctorAppointmentBooking.Services;
+
+namespace DoctorAppointmentBooking.Controllers
+{
+    [Route("/appointments")]
+    public class DoctorTimeSlotController
+    {
+    }
+}
