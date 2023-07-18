b3d5e19f93fc14655404567e51db50e3acc6fdf6
huangshengsimtech <huangsheng.sg@gmail.com>
2023-07-18 16:06:11 +0800

    Refactor Repositories.

---
+++ Management.Infrastructure/Repositories/AppointmentInMemoryRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Management.Domain.Contracts;
+using Management.Domain.Entities;
+
+
+namespace Management.Infrastructure.Repositories
+{
+    public class AppointmentInMemoryRepo : IAppointmentRepository
+    {
+        private static readonly List<Appointment> Appointments = new();
+
+        public async Task Add(Appointment appointment)
+        {
+            Appointments.Add(appointment);
+        }
+
+        public Task<Appointment?> GetById(Guid id)
+        {
+            return Task.FromResult(Appointments.SingleOrDefault(x => x.Id == id));
+        }
+    }
+}
---
+++ Management.Infrastructure/Repositories/DoctorTimeSlotInMemoryRepo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Management.Domain.Contracts;
+using Management.Domain.Entities;
+
+
+namespace Management.Infrastructure.Repositories
+{
+    public class DoctorTimeSlotInMemoryRepo : IDoctorTimeSlotRepository
+    {
+        private static readonly List<DoctorTimeSlot> DoctorTimeSlots = new();
+
+        public async Task Add(DoctorTimeSlot doctorTimeSlot)
+        {
+            DoctorTimeSlots.Add(doctorTimeSlot);
+        }
+
+        public Task<DoctorTimeSlot?> GetById(Guid id)
+        {
+            return Task.FromResult(DoctorTimeSlots.SingleOrDefault(x => x.Id == id));
+        }
+        public  Task<List<DoctorTimeSlot>> GetByDoctorIdAsync(Guid doctorId)
+        {
+            return Task.FromResult(DoctorTimeSlots.Where(d => d.DoctorId == doctorId).ToList());
+        }
+        public  Task<List<DoctorTimeSlot>> GetAvailableTimeSlotsAsync()
+        {
+            return Task.FromResult(DoctorTimeSlots.Where(d => !d.IsReserved).ToList());
+        }
+        public  Task<bool> DoesTimeSlotExist(DateTime time)
+        {
+            return Task.FromResult(DoctorTimeSlots.Any(d => d.Time == time));
+        }
+        public  Task UpdateDoctorTimeSlotReservedStatus(Guid id)
+        {
+            // This is a dummy function for compiling purposes.
+            return Task.FromResult(DoctorTimeSlots.Any(d => d.Id == id));
+
+        }
+    }
+
+}
