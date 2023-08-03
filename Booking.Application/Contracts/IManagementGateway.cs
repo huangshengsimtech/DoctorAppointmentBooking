61734e5a7f4d95fe5c1c9e641a315461c09d7cd6
huangshengsimtech <huangsheng.sg@gmail.com>
2023-08-03 22:03:15 +0800

    Add ManagementGateway

---
+++ Booking.Application/Contracts/IManagementGateway.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementInquiry.Application.Contracts
+{
+    public interface IManagementGateway
+    {
+        Task<Product?> GetProductBySku(string sku);
+    }
+}
---
+++ ManagementInquiry.Infrastructure/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ManagementInquiry.Application.Contracts;
+using ManagementInquiry.Domain;
+
+namespace ManagementInquiry.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddOrdersInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<ICatalogGateway, CatalogGateway>();
+            services.AddTransient<ICartRepo, CartRepoInMemory>();
+            return services;
+        }
+    }
+}
